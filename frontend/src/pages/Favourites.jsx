c3522fdca556352ecc06d914874bdfc5296757f9
MADHESH K <madheshk7239087@gmail.com>
2025-05-03 13:12:58 +0530

    add navbar, then improved the code a bit

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,11 @@
+function Favourites(){
+    return <div>
+        <h1>Favourites page</h1>
+        <p>
+            No  favourites added yet. <br/>
+            <a href="/">Go to Home</a> to add some movies to your favourites.
+        </p>
+    </div>
+}
+
+export default Favourites
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,35 @@
 import MovieCard from "../components/MovieCard";
+import {useState} from "react";
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies=[
-        { id: 1, title: "Movie 1", release_date: "2023-01-01" },
-        { id: 2, title: "Movie 2", release_date: "2023-02-01" },
-        { id: 3, title: "Movie 3", release_date: "2023-03-01" },
-        { id: 4, title: "Movie 4", release_date: "2023-04-01" },
-        { id: 5, title: "Movie 5", release_date: "2023-05-01" },
+        { id: 1, title: "Interstellar", release_date: "2023-01-01" },
+        { id: 2, title: "Inception", release_date: "2023-02-01" },
+        { id: 3, title: "The Matrix", release_date: "2023-03-01" },
+        { id: 4, title: "Pursuit of Happyness", release_date: "2023-04-01" },
+        { id: 5, title: "Forest Gump", release_date: "2023-05-01" },
     ]
 
-    const handleSearch = () => {};
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
 
     return (
         <div className="home">
             <form onSubmit={handleSearch} className="serach-form">
-                <input type="text" placeholder="Search for movies..." className="search-input" />
+                <input type="text" placeholder="Search for movies..." className="search-input" 
+                value={searchQuery} 
+                onChange={(e) => setSearchQuery(e.target.value)} />
                 <button type="submit" className="search-button">Search</button>
             </form>
 
             <div className="movies-grid">
                 {movies.map((movie) => (
-                    <MovieCard movie={movie} key = {movie.id} />
+                    <MovieCard movie={movie} key = {movie.id} /> 
                 ))}
             </div>
         </div>
